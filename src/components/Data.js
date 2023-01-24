d3e9b8ecaa88071338795c2bf09ff9cc6e726055
Apon wuz here <frost_bird@hotmail.com>
2023-01-24 09:51:34 -0600

    updated project technologies

--- src/components/Data.js
+++ src/components/Data.js
@@ -5,7 +5,7 @@ const portfolioData = [
     image: require("../assets/movie2.png"),
     github: "https://github.com/Apom311/movie-project",
     deploy: "https://apom311.github.io/movie-project/",
-    topics: "html javascript css",
+    topics: "html, basic javascript, css",
   },
   {
     id: 2,
@@ -13,15 +13,15 @@ const portfolioData = [
     image: require("../assets/unicorn-blog.png"),
     github: "https://github.com/Apom311/social-Unicorn",
     deploy: "https://vast-mesa-21873.herokuapp.com/",
-    topics: "html javascript css handlebars faker MVC",
+    topics: "MVC layout, handlebars, faker",
   },
   {
     id: 3,
-    name: "Wellness Tracker (",
+    name: "Wellness Tracker",
     image: require("../assets/wellness.png"),
     github: "https://github.com/Apom311/wellness-app",
     deploy: "https://peaceful-basin-12734.herokuapp.com/",
-    topics: "used technologies",
+    topics: "React, MongoDB, ",
   },
 ];
 
