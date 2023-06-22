a7661081acd10682e29f055ef52a55e0fd367998
Apon wuz here <frost_bird@hotmail.com>
2023-06-22 13:03:49 -0500

    updated about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,12 +7,25 @@ function About() {
       <section id="about-section">
         <h1>about: Ryan Kinslow</h1>
         <p>
-          Looking for a change in carrer path, Graduate of the Vanderbilt
-          Fullstack Bootcamp 11/22.
+          Passionate and driven software developer with a strong desire to
+          embark on a new career path.
         </p>
         <p>
-          Fresh Software Developer, experienced in HTML, CSS, JavaScript, React,
-          MongoDB, but most importantly, a desire to learn more.
+          I am a proud graduate of the Vanderbilt Fullstack Bootcamp, completed
+          in November of 2022, where over the course of 6 months I gained
+          hands-on experience and expertise in the MERN stack (MongoDB,
+          Express.js, React, Node.js).
+        </p>
+        <p>
+          As a fresh software developer, I am eager to continue expanding my
+          knowledge and skills while contributing to impactful projects.
+        </p>
+        <p>
+          I thrive in collaborative environments and enjoy tackling complex
+          challenges with innovative solutions. With a growth mindset and a
+          dedication to lifelong learning, I am excited to take on new
+          opportunities and contribute to the ever-evolving world of software
+          development.
         </p>
       </section>
       <Contact />
